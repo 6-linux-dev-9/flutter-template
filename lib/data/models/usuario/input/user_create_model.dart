b075826781e0e13e735ef0619f453b2f7a6beb06
6fernando9 <fernandopadilla170@gmail.com>
2025-11-07 21:17:26 -0400

    feat: iniciando repo vaico

--- lib/data/models/usuario/input/user_create_model.dart
+++
@@ -1,8 +0,0 @@
-class UsuarioCreateModel {
-  final String nombre;
-  final String email;
-  UsuarioCreateModel({required this.nombre, required this.email});
-  factory UsuarioCreateModel.fromJson(Map<String, dynamic> j) =>
-      UsuarioCreateModel(nombre: j['nombre'], email: j['email']);
-  Map<String, dynamic> toJson() => {'nombre': nombre, 'email': email};
-}
--- lib/data/models/usuario/input/user_update_model.dart
+++
@@ -1,9 +0,0 @@
-class UsuarioUpdateModel {
-  final String nombre;
-  final String email;
-  final String estado;
-  UsuarioUpdateModel({required this.nombre, required this.email, required this.estado});
-  factory UsuarioUpdateModel.fromJson(Map<String, dynamic> j) =>
-      UsuarioUpdateModel(nombre: j['nombre'], email: j['email'], estado: j['estado'] ?? '');
-  Map<String, dynamic> toJson() => {'nombre': nombre, 'email': email,'estado': estado.toLowerCase()};
-}
