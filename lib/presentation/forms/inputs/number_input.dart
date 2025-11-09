4d8ce0bd9c39a56242df83a7f317130219c2f76f
6fernando9 <fernandopadilla170@gmail.com>
2025-11-08 21:06:09 -0400

    fix: solucionando tema de componente numerico

--- lib/presentation/forms/inputs/number_input.dart
+++ lib/presentation/forms/inputs/number_input.dart
@@ -67,13 +67,24 @@ class NumberInput extends StatelessWidget {
         signed: false,
       ),
       inputFormatters: [
+        // Permite números y punto o coma
         FilteringTextInputFormatter.allow(RegExp(r'[0-9]+[,.]?[0-9]*')),
+        // Mantiene posición del cursor incluso al reemplazar comas por puntos
         TextInputFormatter.withFunction((oldV, newV) {
-          final t = newV.text.replaceAll(',', '.');
-          return newV.copyWith(
-            text: t,
-            selection: TextSelection.collapsed(offset: t.length),
-          );
+          if (newV.text.contains(',')) {
+            final cursorPos = newV.selection.baseOffset;
+            final replacedText = newV.text.replaceAll(',', '.');
+            final diff = newV.text.length - replacedText.length;
+            final newCursorPos = (cursorPos - diff).clamp(
+              0,
+              replacedText.length,
+            );
+            return newV.copyWith(
+              text: replacedText,
+              selection: TextSelection.collapsed(offset: newCursorPos),
+            );
+          }
+          return newV;
         }),
       ],
       decoration: InputDecoration(
@@ -81,17 +92,8 @@ class NumberInput extends StatelessWidget {
         hintText: hint,
         prefixIcon: const Icon(Icons.pin_outlined),
       ),
-      // sin validator obligatorio
-      // onChanged: (v) {
-      //   if (v.trim().isEmpty) {
-      //     controller.text = '0';
-      //     controller.selection = TextSelection.fromPosition(
-      //       TextPosition(offset: controller.text.length),
-      //     );
-      //   }
-      // },
       onTap: () {
-        // Si el campo tiene un "0" de precarga, se limpia visualmente
+        // Si el campo tiene un valor 0 o 0.0 inicial, se limpia
         if (controller.text == '0' || controller.text == '0.0') {
           controller.clear();
         }
